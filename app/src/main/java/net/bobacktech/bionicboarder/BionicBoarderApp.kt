aa256928c2e726ad2cdb3a7e23b1396069cd0e24
bobacktech <bobacktech@gmail.com>
2024-12-28 09:19:34 -0800

    BB-4: Added a barebones Application class for the android app.

---
+++ app/src/main/java/net/bobacktech/bionicboarder/BionicBoarderApp.kt
@@ -0,0 +1,11 @@
+package net.bobacktech.bionicboarder
+
+import android.app.Application
+
+class BionicBoarderApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
