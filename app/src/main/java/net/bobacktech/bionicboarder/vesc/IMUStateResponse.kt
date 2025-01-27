926db72e5d023e09a509046762889908ad733d74
bobacktech <bobacktech@gmail.com>
2025-01-26 19:38:47 -0800

    BB-8: Added the quaternion and z-axis acceleration fields in IMU response class.

--- app/src/main/java/net/bobacktech/bionicboarder/vesc/IMUStateResponse.kt
+++ app/src/main/java/net/bobacktech/bionicboarder/vesc/IMUStateResponse.kt
@@ -4,6 +4,7 @@ abstract class IMUStateResponse : Response() {
 
     abstract val accelX: Float
     abstract val accelY: Float
+    abstract val accelZ: Float
     abstract val roll: Float
     abstract val pitch: Float
     abstract val yaw: Float
@@ -13,5 +14,9 @@ abstract class IMUStateResponse : Response() {
     abstract val magX: Float
     abstract val magY: Float
     abstract val magZ: Float
+    abstract val quat1: Float
+    abstract val quat2: Float
+    abstract val quat3: Float
+    abstract val quat4: Float
 
 }
