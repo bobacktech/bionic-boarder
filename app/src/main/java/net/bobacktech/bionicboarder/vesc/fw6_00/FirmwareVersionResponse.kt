8555095dd57ab215ba55ff7d0a3a732068630015
bobacktech <bobacktech@gmail.com>
2025-01-31 15:24:45 -0800

    BB-9: Defined the populateImpl method for the firmware response.

--- app/src/main/java/net/bobacktech/bionicboarder/vesc/fw6_00/FirmwareVersionResponse.kt
+++ app/src/main/java/net/bobacktech/bionicboarder/vesc/fw6_00/FirmwareVersionResponse.kt
@@ -17,7 +17,19 @@ class FirmwareVersionResponse : FirmwareVersionResponse() {
     override val versionMajor: String by lazy { _versionMajor }
     override val versionMinor: String by lazy { _versionMinor }
 
+    protected val payloadDataStartIndex: Int = 3
+
     override fun populateImpl(responsePacket: UByteArray) {
-        TODO("Not yet implemented")
+        var index = payloadDataStartIndex
+        _versionMajor = (item<UByte>(responsePacket, index++)!!).toString()
+        _versionMinor = (item<UByte>(responsePacket, index++)!!).toString()
+        val hwName: String = item(responsePacket, index)!!
+        index += hwName.length
+        index += 15
+        val hwType: UByte = item(responsePacket, index)!!
+        _versionDescription = "Firmware Version: $_versionMajor.$_versionMinor\n" +
+                "HW Name: $hwName\n" +
+                "HW Type: $hwType"
     }
+
 }
