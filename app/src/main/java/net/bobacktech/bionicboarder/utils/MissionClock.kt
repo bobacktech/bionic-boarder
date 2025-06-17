9e010ef5a33a0b9119b08562cc041d238ddf4694
bobacktech <bobacktech@gmail.com>
2025-06-17 16:29:43 -0700

    BB-66: Added the MissionClock class definition.

---
+++ app/src/main/java/net/bobacktech/bionicboarder/utils/MissionClock.kt
@@ -0,0 +1,26 @@
+package net.bobacktech.bionicboarder.utils
+
+import android.os.SystemClock
+
+
+/**
+ * This class is used to track the elapsed time of a Rider's mission.
+ */
+class MissionClock {
+
+    private var startTime: Long = 0
+
+    /**
+     * Sets the start time to the current elapsed real time.
+     */
+    fun start() {
+        startTime = SystemClock.elapsedRealtime()
+    }
+
+    /**
+     * Computes the elapsed time since the mission started.
+     */
+    fun invoke(): Long {
+        return (SystemClock.elapsedRealtime() - startTime)
+    }
+}
