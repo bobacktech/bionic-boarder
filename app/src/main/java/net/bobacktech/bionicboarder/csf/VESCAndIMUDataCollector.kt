58e711e725199f30111a5580917434215ce49887
bobacktech <bobacktech@gmail.com>
2025-06-20 15:19:56 -0700

    BB-59: Added a draft of the collector class.

--- app/src/main/java/net/bobacktech/bionicboarder/csf/VESCAndIMUDataCollector.kt
+++ app/src/main/java/net/bobacktech/bionicboarder/csf/VESCAndIMUDataCollector.kt
@@ -1,4 +1,40 @@
 package net.bobacktech.bionicboarder.csf
 
-class VESCAndIMUDataCollector {
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.isActive
+import net.bobacktech.bionicboarder.utils.MissionClock
+import net.bobacktech.bionicboarder.vesc.CommandProducer
+import net.bobacktech.bionicboarder.vesc.Connector
+import net.bobacktech.bionicboarder.vesc.IMUStateResponse
+import net.bobacktech.bionicboarder.vesc.StateResponse
+import net.bobacktech.bionicboarder.vesc.fw6_00.IMUStateReponse
+import java.util.concurrent.ConcurrentLinkedDeque
+
+class VESCAndIMUDataCollector(
+    private val connector: Connector,
+    private val mc: MissionClock,
+    private val vescBuffer: ConcurrentLinkedDeque<Pair<StateResponse, Long>>,
+    private val imuBuffer: ConcurrentLinkedDeque<Pair<IMUStateResponse, Long>>
+) {
+
+    suspend fun collectData() {
+        while (currentCoroutineContext().isActive) {
+            try {
+                val stateResponse: StateResponse =
+                    connector.requestResponse(CommandProducer.CommandChoice.STATE)
+                val imuStateResponse: IMUStateReponse =
+                    connector.requestResponse(CommandProducer.CommandChoice.IMU_STATE)
+                val ts = mc()
+                vescBuffer.add(Pair(stateResponse, ts))
+                imuBuffer.add(Pair(imuStateResponse, ts))
+            } catch (e: Connector.ResponseTimeoutException) {
+                // Log the exception or handle it as needed
+                continue
+            } catch (e: Exception) {
+                // Log the exception or handle it as needed
+                // Notify the user that error occurred and end the riding mission
+                break
+            }
+        }
+    }
 }
